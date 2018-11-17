14dfe8cce5deed28762d72a2e7ae5afada27499a
manorgit <39232258+manorgit@users.noreply.github.com>
2018-11-16 20:58:47 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,17 +6,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+         
           <p>
-            Edit <code>src/App.js</code> and save to reload Mano.
+          Reach Advertising Agency.. Here we go
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+      <p>
+      Coming soon
+      </p>
+         
           </a>
         </header>
       </div>
